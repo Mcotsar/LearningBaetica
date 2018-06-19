4231e69082ebaf093d28bb986042aafd05a3aad1
Maria <mcotsar@gmail.com>
2018-06-19 10:25:39 +0200

    licen

--- Codes/code.R
+++ Codes/code.R
@@ -1,3 +1,9 @@
+# Copyright (c) 2018 - Xavier Rubio-Campillo & Maria Coto-Sarmiento
+# This file is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version
+# More information see <http://www.gnu.org/licenses/>
 
 library(ggplot2)
 library(MASS)
